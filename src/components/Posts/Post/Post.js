d55c444ee573c4abe7fec50c5e6ca6a72e483d68
Laura P <laura.pociuniene@gmail.com>
2021-01-22 19:28:46 +0200

    playing arround with REST API with using axios

---
+++ src/components/Posts/Post/Post.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// import './Post.css';
+
+const post = (props) => (
+
+    <article className="Post">
+        <div>{props.title}</div>
+        <div>test title</div>
+
+    </article>
+
+
+);
+
+export default post;
--- src/containers/PlaygroundContainer.js
+++ src/containers/PlaygroundContainer.js
@@ -1,13 +1,38 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
+import axios from "axios";
 
-const PlaygroundContainer = () => (
-    <Fragment>
-        <div>
-            <h1>This is my playground container</h1>
-        </div>
+import Post from "../components/Posts/Post/Post";
 
-        
-    </Fragment>
-);
+class PlaygroundContainer extends Component {
+  state = {
+    posts: [],
+  };
+  componentDidMount() {
+    axios
+      .get("https://vitrazai.lt/wp-json/wp/v2/media/287")
+      .then((response) => {
+        //   console.timeLog(this.posts);
+        // console.log(response.data);
+        const posts = response.data;
+        this.setState({ posts: posts });
+        console.log(posts);
+      });
+  }
+
+  render() {
+    // const posts = this.state.posts.map((post) => {
+    //     return <Post key={post.id} title={post.title} />;
+    // //   return this.state[post];
+    // });
+    return (
+      <Fragment>
+        <h1>This is my playground container</h1>
+        <h1>id: {this.state.posts.id}</h1>
+        <h1>id: {this.state.posts.id}</h1>
+        {/* {posts} */}
+      </Fragment>
+    );
+  }
+}
 
 export default PlaygroundContainer;
